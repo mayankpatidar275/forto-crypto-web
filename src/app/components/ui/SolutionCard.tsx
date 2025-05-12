cfb1ac49b9c90bf9ad8bd6c47b00b7f32af4a7fe
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-05-12 22:43:17 +0530

    fix: card responsive

--- src/app/components/ui/SolutionCard.tsx
+++ src/app/components/ui/SolutionCard.tsx
@@ -16,11 +16,13 @@ const SolutionCard: React.FC<SolutionCardProps> = ({
   description,
 }) => {
   return (
-    <div className="flex flex-col gap-4 items-center px-12 py-14 rounded-2xl bg-background-b3 max-w-sm">
+    <div className="flex flex-col gap-4 lg:gap-6 items-center px-12 py-14 rounded-2xl bg-background-b3 max-w-sm">
       <img src={icon} alt={alt} className="w-14 h-14" />
-      <Para1>
-        <h6 className="text-white">{title}</h6>
-      </Para1>
+
+      <h6 className="text-white text-lg lg:text-3xl text-center md:text-xl font-bold max-w-xl mx-auto">
+        {title}
+      </h6>
+
       <Para2>{description}</Para2>
     </div>
   );
