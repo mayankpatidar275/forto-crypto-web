8d977ace754641f00bea0db1c2848b6da4b643f8
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-05-14 18:21:31 +0530

    fix: ValueCardProps type add

--- src/app/components/ui/ValueCard.tsx
+++ src/app/components/ui/ValueCard.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Para from "./Para";
 
-const ValueCard = ({ title, description }: any) => {
+interface ValueCardProps {
+  title: string;
+  description: string;
+  icon: string;
+  alt: string;
+}
+
+const ValueCard = ({ title, description }: ValueCardProps) => {
   return (
     <div className="border-b border-border py-6 pr-2 flex flex-col gap-4">
       <h6 className="text-white text-lg lg:text-3xl md:text-xl font-bold max-w-xl">
