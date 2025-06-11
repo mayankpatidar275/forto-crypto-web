7efefd6211d9d11aa8ae4d904da581cd1360b9a1
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-06-11 22:07:55 +0530

    fix: env in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* YOU MUST ADD ENV HERE*/
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL ?? "",
+  },
   images: {
     domains: ["cdn.prod.website-files.com"],
   },
