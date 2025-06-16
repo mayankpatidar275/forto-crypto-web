f13b3a62260a70396c5a3f35b490869aec8b042e
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-06-16 19:36:40 +0530

    fix: logo in docs

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,7 +1,22 @@
 import { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+import Link from "next/link";
+import { WhiteLogo } from "./assets";
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: "FORTO",
+    // title: "FORTO",
+    children: (
+      <Link href="/" aria-label="Homepage" className="flex-shrink-0">
+        <Image
+          src={WhiteLogo}
+          alt="Logo"
+          className="h-5 sm:h-6 w-auto"
+          width={150}
+          height={50}
+          priority
+        />
+      </Link>
+    ),
   },
 };
