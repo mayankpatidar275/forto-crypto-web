c5773533bac9a8ae0b69bbe41c3a1d1c2263366b
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-06-24 23:55:23 +0530

    test: privy commented

--- src/app/components/layout/AppWrapper.tsx
+++ src/app/components/layout/AppWrapper.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 // import { usePrivy } from "@privy-io/react-auth";
-import Header from "./Header";
 import Footer from "./Footer";
 // import Loader from "../ui/Loader";
 import { usePathname } from "next/navigation";
 import { Toaster } from "react-hot-toast";
+import ConnectBtn from "../ui/ConnectBtn";
 
 export default function AppWrapper({
   children,
@@ -23,10 +23,11 @@ export default function AppWrapper({
   return (
     <>
       <Toaster position="bottom-right" />
-      <div className="glow-top"></div>
+      <ConnectBtn />
+      {/* <div className="glow-top"></div>
       <div className="scan-line"></div>
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow">{children}</main> */}
       <Footer />
     </>
   );
