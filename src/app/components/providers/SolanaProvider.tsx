fbbf48fe2af6b56c116f9dea3691b717fd4a21f0
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-08-05 19:06:48 +0530

    feat: conditional wallet for ios and android

--- src/app/components/providers/SolanaProvider.tsx
+++ src/app/components/providers/SolanaProvider.tsx
@@ -34,23 +34,28 @@ export const SolanaProvider: FC<SolanaProviderProps> = ({ children }) => {
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
-    () => [
-      /**
-       * Wallets that implement either of these standards will be available automatically.
-       *
-       *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
-       *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-       *   - Solana Wallet Standard
-       *     (https://github.com/anza-xyz/wallet-standard)
-       *
-       * If you wish to support a wallet that supports neither of those standards,
-       * instantiate its legacy wallet adapter here. Common legacy adapters can be found
-       * in the npm package `@solana/wallet-adapter-wallets`.
-       */
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      // new UnsafeBurnerWalletAdapter(),
-    ],
+    () => {
+      const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+      return isIOS
+        ? [
+            /**
+             * Wallets that implement either of these standards will be available automatically.
+             *
+             *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+             *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+             *   - Solana Wallet Standard
+             *     (https://github.com/anza-xyz/wallet-standard)
+             *
+             * If you wish to support a wallet that supports neither of those standards,
+             * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+             * in the npm package `@solana/wallet-adapter-wallets`.
+             */
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+            // new UnsafeBurnerWalletAdapter(),
+          ]
+        : [];
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [network]
   );
