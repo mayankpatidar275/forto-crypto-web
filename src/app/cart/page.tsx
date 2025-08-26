e4345bfef414a95907c7e6a0016ff9682e5c7d8d
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-08-26 19:19:13 +0530

    fix: deployment issue

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,7 +6,7 @@ import { useAppContext } from "@/custom-hooks/useAppContext";
 import { useUserLogin } from "@/custom-hooks/useUserLogin";
 import { CartItemType } from "@/types/cart";
 import { NFTWithType } from "@/types/nft";
-import { payNftFeeWithUser } from "@/utils/payNftFeeFrontend";
+// import { payNftFeeWithUser } from "@/utils/payNftFeeFrontend";
 import { usePrivy } from "@privy-io/react-auth";
 import toast from "react-hot-toast";
 import CartItemCard, { CartItemCardProps } from "../components/ui/CartItemCard";
@@ -14,9 +14,9 @@ import { EmptyState } from "../components/ui/EmptyState";
 import { ErrorState } from "../components/ui/ErrorState";
 import Loader from "../components/ui/Loader";
 import { TotalCostCard } from "../components/ui/TotalCostCard";
-import { Connection } from "@solana/web3.js";
+// import { Connection } from "@solana/web3.js";
 import { useWallet } from "@solana/wallet-adapter-react";
-import * as anchor from "@coral-xyz/anchor";
+// import * as anchor from "@coral-xyz/anchor";
 
 const CartPage = () => {
   const { authenticated, ready } = usePrivy();
@@ -26,10 +26,10 @@ const CartPage = () => {
   const buyNftMutation = useBuyNft();
   const { login } = useUserLogin();
 
-  const connection = new Connection(
-    "https://api.devnet.solana.com",
-    "confirmed"
-  );
+  // const connection = new Connection(
+  //   "https://api.devnet.solana.com",
+  //   "confirmed"
+  // );
 
   const wallet = useWallet();
 
@@ -53,11 +53,11 @@ const CartPage = () => {
       // Use toast.promise to manage loading, success, and error
       await toast.promise(
         (async () => {
-          await payNftFeeWithUser({
-            connection,
-            wallet: wallet as unknown as anchor.Wallet,
-            eventName: "test-5",
-          });
+          // await payNftFeeWithUser({
+          //   connection,
+          //   wallet: wallet as unknown as anchor.Wallet,
+          //   eventName: "test-5",
+          // });
 
           await buyNftMutation.mutateAsync({
             userPublicAddress: String(wallet.publicKey),
