ff2cdc60781bdcc5dbfabcdb959c15a561b65f51
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-09-21 23:55:52 +0530

    feat: clerk setup

--- src/app/components/layout/BHeader.tsx
+++ src/app/components/layout/BHeader.tsx
@@ -7,7 +7,14 @@ import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import LoginUser from "../ui/LoginUser";
+// import LoginUser from "../ui/LoginUser";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const navLinks = [
   { href: "/docs", label: "Docs" },
@@ -82,7 +89,18 @@ export default function Header() {
 
           <div className="flex gap-1 sm:gap-4">
             {/* Right buttons */}
-            <LoginUser />
+            {/* <LoginUser /> */}
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
         </div>
       </div>
--- src/app/components/providers/Providers.tsx
+++ src/app/components/providers/Providers.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { ContextProvider } from "./ContextProvider";
 import { ReactQueryClientProvider } from "./ReactQueryClientProvider";
 import { SolanaProvider } from "./SolanaProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const Providers = ({
   children,
@@ -14,24 +15,26 @@ const Providers = ({
   return (
     <ReactQueryClientProvider>
       <ContextProvider>
-        <PrivyProvider
-          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
-          clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID || ""}
-          config={{
-            // Display email and wallet as login methods
-            loginMethods: ["email"],
-            // Customize Privy's appearance in your app
-            appearance: {
-              theme: "light",
-              accentColor: "#facc15",
-              walletList: ["metamask", "coinbase_wallet"],
-              // walletChainType: "ethereum-only",
-              // logo: "",
-            },
-          }}
-        >
-          <SolanaProvider>{children}</SolanaProvider>
-        </PrivyProvider>
+        <ClerkProvider>
+          <PrivyProvider
+            appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
+            clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID || ""}
+            config={{
+              // Display email and wallet as login methods
+              loginMethods: ["email"],
+              // Customize Privy's appearance in your app
+              appearance: {
+                theme: "light",
+                accentColor: "#facc15",
+                walletList: ["metamask", "coinbase_wallet"],
+                // walletChainType: "ethereum-only",
+                // logo: "",
+              },
+            }}
+          >
+            <SolanaProvider>{children}</SolanaProvider>
+          </PrivyProvider>
+        </ClerkProvider>
       </ContextProvider>
     </ReactQueryClientProvider>
   );
