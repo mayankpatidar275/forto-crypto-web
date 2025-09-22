c33d22762860159315024e5de3d3bf66f72a11fd
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-09-22 19:45:01 +0530

    feat: participation form

---
+++ src/app/brand/page.tsx
@@ -0,0 +1,12 @@
+import ParticipationForm from "@/app/components-brand/ParticipationForm";
+import React from "react";
+
+function ParticipatePage() {
+  return (
+    <div className="cp-y">
+      <ParticipationForm></ParticipationForm>
+    </div>
+  );
+}
+
+export default ParticipatePage;
--- src/app/components-brand/ui/BuyNowBtnHero.tsx
+++ src/app/components-brand/ui/BuyNowBtnHero.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function BuyNowBtnHero() {
-  const scrollToSection = (id: string) => {
-    const element = document.getElementById(id);
-    element?.scrollIntoView({ behavior: "smooth" });
-  };
+  const router = useRouter();
+  // const scrollToSection = (id: string) => {
+  //   const element = document.getElementById(id);
+  //   element?.scrollIntoView({ behavior: "smooth" });
+  // };
 
   return (
     <button
-      onClick={() => scrollToSection("get-now")}
+      // onClick={() => scrollToSection("get-now")}
+      onClick={() => router.push("/brand")}
       className="inline-block border-2 border-brand-br2 text-brand-br1 pointer px-6 py-3 rounded-xl text-lg md:text-xl font-semibold hover:bg-brand-br2 hover:text-link transition-colors duration-[400ms] ease-[cubic-bezier(.25,.46,.45,.94)]"
     >
       Join Giveaway
