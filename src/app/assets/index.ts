2ebfda5a80115134ba2b74b8a8b075a184dd6d8c
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-09-26 20:11:19 +0530

    feat: current giveaways, animated 50000

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -6,3 +6,4 @@ export { default as CurrentGiveaways } from "./forto_giveaway_1920x1080_extended
 export { default as CustomersWinMore } from "./ChatGPT Image Sep 21, 2025, 03_53_41 PM.png";
 export { default as BrandTurnsGiveaways } from "./ChatGPT Image Sep 21, 2025, 04_08_04 PM.png";
 export { default as Rocket } from "./rocket.png";
+export { default as Street } from "./6thStreet.svg";
--- src/app/components/CurrentGiveawaysSection.tsx
+++ src/app/components/CurrentGiveawaysSection.tsx
@@ -1,14 +1,54 @@
 import Image from "next/image";
 import Heading2 from "./ui/Heading2";
-import { CurrentGiveaways } from "../assets";
+import { CurrentGiveaways, Street, WhiteLogo } from "../assets";
 
 function CurrentGiveawaysSection() {
   return (
     <section className="cp-x cp-y flex justify-center">
       <div className="flex flex-col justify-center items-center">
-        <Heading2>
-          Current <span className="text-brand-br">Giveaways</span>
-        </Heading2>
+        <Heading2>Current Giveaways</Heading2>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div className="flex gap-4">
+            <div>
+              <Image
+                src={WhiteLogo}
+                alt="Logo"
+                className="h-5 sm:h-8 w-auto"
+                width={150}
+                height={50}
+                priority
+              />
+            </div>
+            <span>x</span>
+            <div>
+              <Image
+                src={Street}
+                alt="6thStreetLogo"
+                className="h-5 sm:h-8 w-auto invert"
+                width={150}
+                height={50}
+                priority
+              />
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="title">
+              50,0000
+              <div className="aurora">
+                <div className="aurora__item"></div>
+                <div className="aurora__item"></div>
+                <div className="aurora__item"></div>
+                <div className="aurora__item"></div>
+              </div>
+            </h1>
+
+            <span className="text-4xl">GIVEAWAY</span>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <span>50% WINNERS, 100% FAIR</span>
+            <span>TRANSPARENT ON BLOCKCHAIN</span>
+          </div>
+        </div>
         <figure className="flex justify-center items-center mb-10 relative overflow-hidden image-gradient-container">
           <Image
             src={CurrentGiveaways}
