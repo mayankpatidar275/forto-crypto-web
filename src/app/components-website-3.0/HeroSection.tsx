5a17f36f6483a6707da9c08a138dffcc375daecc
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-09-26 17:57:57 +0530

    feat: hero sectioin bg image

--- src/app/components-website-3.0/HeroSection.tsx
+++ src/app/components-website-3.0/HeroSection.tsx
@@ -4,28 +4,23 @@ import HeroImageAnimation from "./ui/HeroImageAnimation";
 
 const HeroSection = () => {
   return (
-    <section className="section-hero cp-x">
+    <section className="section-hero cp-x hero-section bg-url[]">
       <div className="mt-2 md:mt-6">
         <div className="block-heading px-6 py-14 text-center flex flex-col items-center">
           <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-brand-br bg-clip-text leading-tight mb-6 max-w-5xl">
             50% WINNERS & 100% FAIR
           </h1>
-
+          <BuyNowBtnHero />
+        </div>
+        <div>
+          <HeroImageAnimation />
           <div className="mb-8">
             <Para1>
               The first blockchain powered giveaway where 50% customers win
               rewards and brands only pay when customers shop
             </Para1>
-            {/* <Para1 className="hidden sm:block">
-              — where jackpots grow bigger, rewards go deeper, and every ticket
-              brings you closer to massive payouts. 🚀💰
-            </Para1> */}
           </div>
-
-          <BuyNowBtnHero />
         </div>
-
-        <HeroImageAnimation />
       </div>
     </section>
   );
