983c8b59c73ecb6ebab142341a6412792b447fd8
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-09-29 18:28:44 +0530

    fix: forto vs traditional desktop responsive

--- src/app/components-website-3.0/FortoVsTraditional.tsx
+++ src/app/components-website-3.0/FortoVsTraditional.tsx
@@ -2,19 +2,17 @@
 import Heading2 from "./ui/Heading2";
 // import Para1 from "./ui/Para1";
 import { Check } from "lucide-react";
-import Image from "next/image";
-import { CustomersWinMore } from "../assets";
 
 const FortoVsTraditional = () => {
   return (
     <section className="cp-x cp-y flex justify-center">
-      <div className="max-w-6xl grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
+      <div className="max-w-6xl gap-10 items-start">
         {/* Right Content */}
         <div className="flex flex-col items-center lg:items-start gap-4 h-full justify-center">
           {/* <Label text="About us" /> */}
           <Heading2 className="">FORTO vs TRADITIONAL GIVEAWAYS</Heading2>
-          <div className="lg:text-left md:flex">
-            <ul className="flex flex-col justify-self-start gap-2">
+          <div className="lg:text-left md:flex w-full">
+            <ul className="flex flex-col lg:flex-row justify-self-start gap-2 lg:gap-8">
               <li className="flex text-very-light-pink items-center">
                 <span className="bg-[#331910] rounded-full h-7 w-7 flex justify-center items-center mr-4">
                   <Check className="text-[#ff8730] p-1" />
@@ -41,21 +39,6 @@ const FortoVsTraditional = () => {
               </li>
             </ul>
           </div>
-        </div>
-
-        {/* Left Content */}
-        <div className="lg:flex flex-col items-center gap-6 hidden">
-          <figure className="flex flex-col justify-center items-center -z-1 w-full mb-10 relative overflow-hidden">
-            <Image
-              src={CustomersWinMore}
-              alt="Decorative shape"
-              width={800} // pick an appropriate width (can adjust)
-              height={600} // pick an appropriate height (can adjust)
-              sizes="(max-width: 767px) 83vw, 500px"
-              className="h-full max-w-full w-auto inline-block"
-              priority={false}
-            />
-          </figure>
         </div>
       </div>
     </section>
