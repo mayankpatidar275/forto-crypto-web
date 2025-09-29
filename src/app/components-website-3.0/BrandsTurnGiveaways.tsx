fbbe1a9bcf4d3d007d6b00f88d3c4bd6e8dde253
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-09-29 18:32:54 +0530

    fix: brands section responsive

--- src/app/components-website-3.0/BrandsTurnGiveaways.tsx
+++ src/app/components-website-3.0/BrandsTurnGiveaways.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-// import Label from "./ui/Label";
-// import Para1 from "./ui/Para1";
-import Image from "next/image";
-import { BrandTurnsGiveaways } from "../assets";
 import SolutionCard from "./ui/SolutionCard";
 import { Coins, Handshake, ShoppingBag, Sparkle } from "lucide-react";
 import Heading4 from "./ui/Heading4";
@@ -10,25 +6,13 @@ import Heading4 from "./ui/Heading4";
 const BrandsTurnGiveaways = () => {
   return (
     <section className="cp-x cp-y flex justify-center brands-section">
-      <div className="max-w-6xl grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
-        {/* Left Content */}
-        <div className="lg:flex flex-col items-center gap-6 hidden">
-          <figure className="flex flex-col justify-center items-center -z-1 w-full mb-10 relative overflow-hidden">
-            <Image
-              src={BrandTurnsGiveaways}
-              alt="Decorative shape"
-              width={800} // pick an appropriate width (can adjust)
-              height={600} // pick an appropriate height (can adjust)
-              sizes="(max-width: 767px) 83vw, 500px"
-              className="h-full max-w-full w-auto inline-block"
-              priority={false}
-            />
-          </figure>
-        </div>
+      <div className="max-w-6xl gap-10 items-start">
         {/* Right Content */}
-        <div className="flex flex-col items-center lg:items-end gap-4 h-full justify-center">
+        <div className="flex flex-col items-center lg:items-start gap-4 h-full justify-center">
           {/* <Label text="About us" /> */}
-          <Heading4 className="">BRANDS: TURN GIVEAWAYS INTO GROWTH</Heading4>
+          <Heading4 className="lg:text-left">
+            BRANDS: TURN GIVEAWAYS INTO GROWTH
+          </Heading4>
           <div className="flex flex-col gap-4">
             <SolutionCard
               title="Only pay when rewards drive sales"
